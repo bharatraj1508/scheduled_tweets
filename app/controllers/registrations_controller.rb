bb864d460298d8b2a4f640fe4f1bfef0f7451d0c
Bharat Raj Verma <bharat.raj1508@gmail.com>
2023-12-27 19:01:56 -0500

    commit to add files

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,6 +8,7 @@ class RegistrationsController < ApplicationController
         # binding.pry
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to root_path, notice: "Seccussfully created account"
         else
             render :new
